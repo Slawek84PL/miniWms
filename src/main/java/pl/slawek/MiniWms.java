74949bf08cb21be8729157889f25aee671448acb
Slawek <slapyt84@gmail.com>
2023-11-09 12:16:46 +0100

    Renamed from main

---
+++ src/main/java/pl/slawek/MiniWms.java
@@ -0,0 +1,11 @@
+package pl.slawek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniWms {
+    public static void main(String[] args) {
+        SpringApplication.run(MiniWms.class, args);
+    }
+}

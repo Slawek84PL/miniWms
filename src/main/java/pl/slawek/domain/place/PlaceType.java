e6079c7c6ebf3a1c54cd6a078da0f732a4daad44
Slawek <slapyt84@gmail.com>
2023-11-11 18:34:48 +0100

    Added PlaceType class

---
+++ src/main/java/pl/slawek/domain/place/PlaceType.java
@@ -0,0 +1,14 @@
+package pl.slawek.domain.place;
+
+public class PlaceType {
+
+    private boolean using;
+
+    public boolean isUsing() {
+        return using;
+    }
+
+    public void setUsing(boolean using) {
+        this.using = using;
+    }
+}

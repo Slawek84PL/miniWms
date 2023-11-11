c16c5dc67ce2b65d7ad4c95e6b3e6721312d53a6
Slawek <slapyt84@gmail.com>
2023-11-11 18:30:39 +0100

    Added Warehouse class

---
+++ src/main/java/pl/slawek/domain/Warehouse.java
@@ -0,0 +1,34 @@
+package pl.slawek.domain;
+
+import pl.slawek.domain.adress.Adress;
+import pl.slawek.domain.pleace.Pleace;
+
+import java.util.List;
+
+public class Warehouse {
+    private String name;
+
+    private Adress adress;
+
+    private List<Pleace> places;
+
+    public Warehouse(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Adress getAdress() {
+        return adress;
+    }
+
+    public void setAdress(Adress adress) {
+        this.adress = adress;
+    }
+}

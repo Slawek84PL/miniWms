e12beb9fc8dc97d436134a3b3d3cebfec7679c8b
Slawek <slapyt84@gmail.com>
2023-11-15 18:49:39 +0100

    Implement Warehouse like entity

--- src/main/java/pl/slawek/domain/warehouse/Warehouse.java
+++ src/main/java/pl/slawek/domain/warehouse/Warehouse.java
@@ -1,5 +1,10 @@
 package pl.slawek.domain.warehouse;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -13,7 +18,12 @@ import pl.slawek.domain.address.Address;
 @Getter
 @Setter
 @EqualsAndHashCode
+@Entity
 public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
 
     @Size(min = 3, max = 4)
     private String shortName;
@@ -21,5 +31,7 @@ public class Warehouse {
     @Size(min = 3, max = 25)
     private String name;
 
-    private Address address;
+//    @OneToOne
+//    private Address address;
+
 }

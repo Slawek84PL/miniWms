c8e64dc51c679c5edca016e873baa70d752fe818
Slawek <slapyt84@gmail.com>
2023-11-15 18:50:28 +0100

    Implement AddressService

---
+++ src/main/java/pl/slawek/domain/address/service/AddressService.java
@@ -0,0 +1,31 @@
+package pl.slawek.domain.address.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.slawek.domain.address.Address;
+import pl.slawek.domain.address.repository.AddressRepository;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class AddressService {
+
+    private final AddressRepository repository;
+
+    public List<Address> getAll() {
+        return repository.findAll();
+    }
+
+    public Address getOne(long id) {
+        return repository.getReferenceById(id);
+    }
+
+    public Long add(Address address) {
+        return repository.save(address).getId();
+    }
+
+    public void delete(long id) {
+        repository.deleteById(id);
+    }
+}

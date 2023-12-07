0633f34117e589e6bbbe81dee9333de0f36ce849
Slawek <slapyt84@gmail.com>
2023-12-07 12:58:17 +0100

    Added place like entity

--- src/main/java/pl/slawek/domain/place/Place.java
+++ src/main/java/pl/slawek/domain/place/Place.java
@@ -1,25 +1,31 @@
 package pl.slawek.domain.place;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import pl.slawek.domain.warehouse.Warehouse;
-import pl.slawek.domain.article.Article;
-
-import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
 public class Place {
+
+    @Id
+    @GeneratedValue
+    private Long id;
 
     private String name;
 
-    private PlaceType placeType;
-
-    private List<Article> articles;
-
+    @ManyToOne
+    @JoinColumn(name = "warehouse_id")
     private Warehouse warehouse;
+
 }

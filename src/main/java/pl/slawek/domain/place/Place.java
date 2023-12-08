9f91c84e0fa14195eeb61927b97b6a32a4bcf611
Slawek <slapyt84@gmail.com>
2023-12-08 21:48:31 +0100

    Added uniqueConstraints

--- src/main/java/pl/slawek/domain/place/Place.java
+++ src/main/java/pl/slawek/domain/place/Place.java
@@ -1,18 +1,16 @@
 package pl.slawek.domain.place;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import pl.slawek.domain.warehouse.Warehouse;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,18 +18,16 @@ import pl.slawek.domain.warehouse.Warehouse;
 @Getter
 @Setter
 @Entity
+@Table(name = "place", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"name", "warehouse_id"})
+})
 public class Place {
 
     @Id
     @GeneratedValue
     private Long id;
 
-    @Column(unique = true)
     @Size(min = 2, max = 15)
     private String name;
-
-    @ManyToOne
-    @JoinColumn(name = "warehouse_id")
-    private Warehouse warehouse;
 
 }

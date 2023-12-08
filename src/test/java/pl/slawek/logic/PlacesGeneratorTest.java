c1e1078fed6cebbf365bbef24609da45e9a5419c
Slawek <slapyt84@gmail.com>
2023-12-08 15:27:00 +0100

    Added tests for places generator

---
+++ src/test/java/pl/slawek/logic/PlacesGeneratorTest.java
@@ -0,0 +1,75 @@
+package pl.slawek.logic;
+
+import org.junit.jupiter.api.Test;
+import pl.slawek.domain.place.Place;
+import pl.slawek.domain.warehouse.Warehouse;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PlacesGeneratorTest {
+
+    private static Warehouse warehouse;
+
+    @Test
+    void ShouldGenerateOnePlace() {
+//        when
+        PlacesGenerator placesGenerator = new PlacesGenerator(1,false, 0,1);
+//        given
+        List<Place> list = placesGenerator.generate(warehouse);
+//        then
+        assertThat(list.size()).isEqualTo(1);
+        assertEquals(list.get(0).getName(), "01-00-01");
+
+    }
+
+    @Test
+    void ShouldGenerateTwoPlaces() {
+//        when
+        PlacesGenerator placesGenerator = new PlacesGenerator(1,false, 0,2);
+//        given
+        List<Place> list = placesGenerator.generate(warehouse);
+//        then
+        assertThat(list.size()).isEqualTo(2);
+        assertEquals(list.get(0).getName(), "01-00-01");
+        assertEquals(list.get(1).getName(), "01-00-02");
+    }
+
+    @Test
+    void ShouldGenerate10Rows() {
+//        when
+        PlacesGenerator placesGenerator = new PlacesGenerator(10,false, 0,1);
+//        given
+        List<Place> list = placesGenerator.generate(warehouse);
+//        then
+        assertThat(list.size()).isEqualTo(10);
+        assertEquals(list.get(0).getName(), "01-00-01");
+        assertEquals(list.get(9).getName(), "10-00-01");
+    }
+
+    @Test
+    void ShouldGenerate10Levels() {
+//        when
+        PlacesGenerator placesGenerator = new PlacesGenerator(1,false, 9,1);
+//        given
+        List<Place> list = placesGenerator.generate(warehouse);
+//        then
+        assertThat(list.size()).isEqualTo(10);
+        assertEquals(list.get(0).getName(), "01-00-01");
+        assertEquals(list.get(9).getName(), "01-09-01");
+    }
+
+    @Test
+    void ShouldGenerate10Places() {
+//        when
+        PlacesGenerator placesGenerator = new PlacesGenerator(1,false, 0,10);
+//        given
+        List<Place> list = placesGenerator.generate(warehouse);
+//        then
+        assertThat(list.size()).isEqualTo(10);
+        assertEquals(list.get(0).getName(), "01-00-01");
+        assertEquals(list.get(9).getName(), "01-00-10");
+    }
+}

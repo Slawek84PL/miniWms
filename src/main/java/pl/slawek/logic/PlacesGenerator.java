b467a93458ec0e64f9f1a32667a78edcbb45912b
Slawek <slapyt84@gmail.com>
2023-12-08 20:03:36 +0100

    Added validation for fields size

--- src/main/java/pl/slawek/logic/PlacesGenerator.java
+++ src/main/java/pl/slawek/logic/PlacesGenerator.java
@@ -1,5 +1,8 @@
 package pl.slawek.logic;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import pl.slawek.domain.place.Place;
 import pl.slawek.domain.warehouse.Warehouse;
@@ -10,21 +13,31 @@ import java.util.List;
 @AllArgsConstructor
 public class PlacesGenerator {
 
+    @Min(1)
+    @Max(99)
     private int row;
+
     private boolean rowAsLetter;
+
+    @Min(0)
+    @Max(99)
     private int level;
+
+    @Min(1)
+    @Max(99)
     private int column;
 
     public List<Place> generate(Warehouse warehouse) {
         List<Place> places = new ArrayList<>();
 
         for (int i = 1; i <= row; i++) {
+
             for (int j = 0; j <= level; j++) {
+
                 for (int k = 1; k <= column; k++) {
                     places.add(new Place(null, String.format("%02d-%02d-%02d", i, j, k), warehouse));
                 }
             }
-
         }
 
         return places;

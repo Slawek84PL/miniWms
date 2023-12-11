f44676c5068dd3c46c59f7c49b29f78e7338bcac
Slawek <slapyt84@gmail.com>
2023-12-11 22:24:58 +0100

    Added first address to database

---
+++ src/main/resources/db/address/02_first_address.sql
@@ -0,0 +1,3 @@
+insert into address (number, post_code, id, country, city, street)
+    VALUES (10, '42230', 1, 'polska', 'Goczałkowice-Zdrój', 'Bór II'),
+           (11, '43240', 2, 'PL', 'Pszczyna', 'Zakazana');

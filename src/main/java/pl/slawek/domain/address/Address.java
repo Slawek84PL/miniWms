db636576bfe3b42b2ef0457b46aa7b31f2849af3
Slawek <slapyt84@gmail.com>
2023-12-11 22:10:44 +0100

    Added validation

--- src/main/java/pl/slawek/domain/address/Address.java
+++ src/main/java/pl/slawek/domain/address/Address.java
@@ -3,6 +3,8 @@ package pl.slawek.domain.address;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,10 +21,22 @@ public class Address {
     @GeneratedValue
     private Long id;
 
+    @NotBlank
+    @Size(min = 2, max = 20)
     private String country;
+
+    @NotBlank
+    @Size(min = 5, max = 5)
     private String postCode;
+
+    @NotBlank
+    @Size(min = 2, max = 25)
     private String city;
+
+    @NotBlank
+    @Size(min = 2, max = 50)
     private String street;
+
     private int number;
 
 }

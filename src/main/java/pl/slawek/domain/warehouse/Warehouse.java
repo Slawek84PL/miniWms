d9e546d20b47498818f2e28679ea2585d1a1b77b
Slawek <slapyt84@gmail.com>
2023-12-17 14:14:30 +0100

    Added 2 side relation

--- src/main/java/pl/slawek/domain/warehouse/Warehouse.java
+++ src/main/java/pl/slawek/domain/warehouse/Warehouse.java
@@ -1,26 +1,31 @@
 package pl.slawek.domain.warehouse;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import pl.slawek.domain.address.Address;
+import pl.slawek.domain.place.Place;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode
 @Entity
 public class Warehouse {
 
@@ -34,6 +39,11 @@ public class Warehouse {
 
     @Size(min = 3, max = 25)
     private String name;
+
+
+    @Getter(AccessLevel.PACKAGE)
+    @OneToMany(mappedBy = "warehouse", cascade = CascadeType.REMOVE)
+    private List<Place> places;
 
     @OneToOne
     private Address address;

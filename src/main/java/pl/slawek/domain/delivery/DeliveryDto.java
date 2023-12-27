3eeb7db89bdd6b625fb9b0de084da32dec1c85de
Slawek <slapyt84@gmail.com>
2023-12-27 21:12:20 +0100

    Fixed DeliveryDto

--- src/main/java/pl/slawek/domain/delivery/DeliveryDto.java
+++ src/main/java/pl/slawek/domain/delivery/DeliveryDto.java
@@ -2,29 +2,25 @@ package pl.slawek.domain.delivery;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import pl.slawek.domain.delivery.position.Position;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@Setter
 @RequiredArgsConstructor
 public class DeliveryDto {
 
+    private long deliveryId;
     private String warehouse;
     private String companyName;
 
-    private List<List<Position>> positions = new ArrayList<>();
-    
-    public DeliveryDto(List<Delivery> source) {
-        this.warehouse = source.get(0).getWarehouse().getName();
-        this.companyName = source.get(0).getCompany().getName();
+    private List<Position> positions = new ArrayList<>();
 
-        for (Delivery delivery : source) {
-//            delivery.setPositions(delivery.getPositions());
-            this.positions.add(delivery.getPositions());
-        }
+    public DeliveryDto(Delivery source) {
+        this.deliveryId = source.getId();
+        this.warehouse = source.getWarehouse().getName();
+        this.companyName = source.getCompany().getName();
+        this.positions = source.getPositions();
     }
 }

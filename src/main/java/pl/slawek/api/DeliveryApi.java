3159d1669d471924bcd0127eb3eff9a79d2fe827
Slawek <slapyt84@gmail.com>
2023-12-27 21:08:30 +0100

    Api returned DeliveryDto

--- src/main/java/pl/slawek/api/DeliveryApi.java
+++ src/main/java/pl/slawek/api/DeliveryApi.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.slawek.domain.delivery.Delivery;
 import pl.slawek.domain.delivery.DeliveryDto;
 import pl.slawek.domain.delivery.service.DeliveryService;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,22 +21,22 @@ public class DeliveryApi {
     private final DeliveryService service;
 
     @GetMapping
-    public ResponseEntity<DeliveryDto> getAll(@PathVariable long warehouseId) {
-        return new ResponseEntity<>(service.getAllDeliveryForWarehouse(warehouseId), HttpStatus.OK);
+    public ResponseEntity<List<DeliveryDto>> getAll(@PathVariable long warehouseId) {
+        return new ResponseEntity<>(service.getAllForWarehouse(warehouseId), HttpStatus.OK);
     }
 
     @GetMapping("{deliveryId}")
-    public ResponseEntity<Delivery> getOneDeliveryForWarehouse(@PathVariable long warehouseId, @PathVariable long deliveryId) {
+    public ResponseEntity<DeliveryDto> getOneDeliveryForWarehouse(@PathVariable long warehouseId, @PathVariable long deliveryId) {
         return new ResponseEntity<>(service.getOneDeliveryForWarehouse(warehouseId, deliveryId), HttpStatus.CREATED);
     }
 
     @GetMapping("/company/{companyId}")
-    public ResponseEntity<DeliveryDto> getAllForWarehouseAndCompany(@PathVariable long warehouseId, @PathVariable long companyId) {
+    public ResponseEntity<List<DeliveryDto>> getAllForWarehouseAndCompany(@PathVariable long warehouseId, @PathVariable long companyId) {
         return new ResponseEntity<>(service.getAllForWarehouseAndCompany(warehouseId, companyId), HttpStatus.CREATED);
     }
 
     @PostMapping("/company/{companyId}")
-    public ResponseEntity<Delivery> add(@PathVariable long warehouseId, @PathVariable long companyId) {
+    public ResponseEntity<DeliveryDto> add(@PathVariable long warehouseId, @PathVariable long companyId) {
         return new ResponseEntity<>(service.add(warehouseId, companyId), HttpStatus.CREATED);
     }
 }

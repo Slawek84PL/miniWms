937eeb7a53cc0ffba6da9fea4ff39b06a7028749
Slawek <slapyt84@gmail.com>
2024-01-10 18:10:58 +0100

    Added SupperClass for company

--- src/main/java/pl/slawek/domain/warehouse/Warehouse.java
+++ src/main/java/pl/slawek/domain/warehouse/Warehouse.java
@@ -1,21 +1,13 @@
 package pl.slawek.domain.warehouse;
 
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import pl.slawek.domain.address.Address;
+import pl.slawek.domain.BaseCompanyData;
 import pl.slawek.domain.place.Place;
 
 import java.util.List;
@@ -25,37 +17,8 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
-public class Warehouse {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Size(min = 3, max = 4)
-    @Column(unique = true)
-    private String shortName;
-
-    @Size(min = 3, max = 25)
-    private String name;
+public class Warehouse extends BaseCompanyData {
 
     @OneToMany(mappedBy = "warehouse", cascade = CascadeType.REMOVE)
     private List<Place> places;
-
-    @OneToOne
-    private Address address;
-
-    @PrePersist
-    private void afterCreate() {
-        upperShortName();
-    }
-
-    @PreUpdate
-    private void afterUpdate() {
-        upperShortName();
-    }
-
-    private void upperShortName() {
-        this.shortName = shortName.toUpperCase();
-    }
-
 }

bff8ee56b386b3806389b4a3d519a971303e85e3
Slawek <slapyt84@gmail.com>
2024-01-18 19:31:44 +0100

    Created address list view for admin

---
+++ src/main/java/pl/slawek/gui/admin/AddressAdminViewController.java
@@ -0,0 +1,39 @@
+package pl.slawek.gui.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.slawek.domain.address.Address;
+import pl.slawek.domain.address.service.AddressService;
+
+@Controller
+@RequestMapping("admin/address")
+public class AddressAdminViewController {
+
+   private final AddressService addressService;
+
+    public AddressAdminViewController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+    @GetMapping
+    public String indexView(Model model) {
+        model.addAttribute("addresses", addressService.getAll());
+        return "admin/address/index";
+    }
+
+    @GetMapping("add")
+    public String addView(Model model) {
+        model.addAttribute("address", new Address());
+        return "admin/address/add";
+    }
+
+    @PostMapping("add")
+    public String add(@ModelAttribute("address") Address address) {
+        addressService.add(address);
+        return "redirect:/admin/address";
+    }
+}

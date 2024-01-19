9c2e6f19054f0bfaf02e96ac678e9642cc41af34
Slawek <slapyt84@gmail.com>
2024-01-19 11:36:55 +0100

    Added delete button

--- src/main/java/pl/slawek/gui/admin/AddressAdminViewController.java
+++ src/main/java/pl/slawek/gui/admin/AddressAdminViewController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.slawek.domain.address.Address;
@@ -34,6 +35,12 @@ public class AddressAdminViewController {
     @PostMapping("add")
     public String add(@ModelAttribute("address") Address address) {
         addressService.add(address);
+        return "redirect:/admin/address";
+    }
+
+    @GetMapping("delete/{addressId}")
+    public String delete(@PathVariable long addressId) {
+        addressService.delete(addressId);
         return "redirect:/admin/address";
     }
 }

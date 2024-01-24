225fc90f47755557f698ea1dbac52959852dc1cd
Slawek <slapyt84@gmail.com>
2024-01-24 14:24:39 +0100

    Added edit view

--- src/main/java/pl/slawek/gui/admin/CompanyAdminViewController.java
+++ src/main/java/pl/slawek/gui/admin/CompanyAdminViewController.java
@@ -1,24 +1,61 @@
 package pl.slawek.gui.admin;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.slawek.domain.company.entity.Company;
 import pl.slawek.domain.company.service.CompanyService;
 
 @Controller
 @RequestMapping("admin/companies")
 public class CompanyAdminViewController {
 
-    private final CompanyService companyService;
+    private final CompanyService service;
 
-    public CompanyAdminViewController(CompanyService companyService) {
-        this.companyService = companyService;
+    public CompanyAdminViewController(CompanyService service) {
+        this.service = service;
     }
 
     @GetMapping
     public String indexView(Model model) {
-        model.addAttribute("companies", companyService.getAll());
+        model.addAttribute("companies", service.getAll());
         return "admin/company/index";
+    }
+
+    @GetMapping("add")
+    public String addView(Model model) {
+        model.addAttribute("company", new Company());
+        return "admin/company/edit";
+    }
+
+    @PostMapping("save")
+    public String add(@Valid @ModelAttribute("company") Company company,
+                      BindingResult bindingResult, Model model) {
+
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("company", company);
+            return "admin/company/edit";
+        }
+
+        service.add(company);
+        return "redirect:/admin/companies";
+    }
+
+    @GetMapping("delete/{companyId}")
+    public String delete(@PathVariable long companyId) {
+        service.delete(companyId);
+        return "redirect:/admin/companies";
+    }
+
+    @GetMapping("edit/{companyId}")
+    public String edit(@PathVariable long companyId, Model model) {
+        model.addAttribute("company", service.getOne(companyId));
+        return "admin/company/edit";
     }
 }
